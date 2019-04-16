1b7c985164f1eaee8d6ee4563fb7e4de33a9fbe5
lemri <league@WTS-IM-17.attlocal.net>
2019-04-16 16:56:59 -0700

    random

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,13 +8,16 @@ import java.util.Random;
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
-		int r = 0;
-		
+	
+	
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		int r = 0;
+		Random random = new Random();
+		int number = random .nextInt((75-25)+1)+25;
+		System.out.println(number);
+System.out.println(number);
 		//4. Limit the random number between 0 and 100
 
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
@@ -26,4 +29,5 @@ public class IntroToRandom {
 		//2. Run the program.  What number appears in the console?
 		//   Run it again. Is the number the same?
 	}
+
 }
--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -6,6 +6,7 @@ package random;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -81,7 +82,8 @@ public class RockPaperScissors extends JPanel implements ActionListener{
 	    
         //2. Change the value of opponentSelection to be a random number between 0 and 2; 
         int opponentSelection = 0;
-        
+		Random random = new Random();
+		opponentSelection = random.nextInt(3);
         //3. Run the program again. Is the result different?
  
         int selection = 0;
